a9552bee5fc32291315e3e0b93b471e5d8854b8e
gonbaez <gon.baez@gmail.com>
2024-03-26 20:49:52 +0000

    Added about route

--- server.js
+++ server.js
@@ -11,6 +11,44 @@ theSimpsons.forEach((char, index) => {
 // handle reques to static files
 app.use(express.static("public"));
 
+app.get("/about/:country", (req, res) => {
+  // res.send("<h1>Hello World!</h1>"); // This is how websites used to work (sending everything from the backend)
+
+  switch (req.params.country) {
+    case "france":
+      res.send(`<div>
+      <h1>French head office</h1>
+      <p>123 Paris Road</p>
+      <p>Paris</p>
+      <p>SW1 1AB</p>
+      </div>`);
+      break;
+    case "spain":
+      res.send(`<div>
+      <h1>Spanish head office</h1>
+      <p>123 Madrid Road</p>
+      <p>Madrid</p>
+      <p>SW1 1AB</p>
+      </div>`);
+      break;
+    case "germany":
+      res.send(`<div>
+      <h1>German head office</h1>
+      <p>123 Berlin Road</p>
+      <p>Berlin</p>
+      <p>SW1 1AB</p>
+      </div>`);
+      break;
+    default:
+      res.send(`<div>
+      <h1>Uk head office</h1>
+      <p>123 London Road</p>
+      <p>London</p>
+      <p>SW1 1AB</p>
+      </div>`);
+  }
+});
+
 // handle requests for dynamic data
 app.get("/", (req, res) => {
   res.send("Hello from the backend!");
